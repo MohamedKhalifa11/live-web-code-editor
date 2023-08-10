ba4dfd814bbe73622c7e80e3ec7a27fa3d7ab5cb
MohamedKhalifa11 <mkhkun11@gmail.com>
2023-08-10 12:53:45 +0300

    Auto Save & Auto Load

--- js/app.js
+++ js/app.js
@@ -1,37 +1,70 @@
-document.getElementById("htmlCode").value="<div>\n\n</div>";
-document.getElementById("cssCode").value="<style>\n\n</style>";
-document.getElementById("jsCode").value="<script>\n\n</script>";
+document.getElementById("htmlCode").value = "<div>\n\n</div>";
+document.getElementById("cssCode").value = "<style>\n\n</style>";
+document.getElementById("jsCode").value = "<script>\n\n</script>";
 
-function showPreview(){
-    var htmlCode = document.getElementById("htmlCode").value;
-    var cssCode = ""+document.getElementById("cssCode").value+"";
-    var jsCode = ""+document.getElementById("jsCode").value+"";
-    var frame = document.getElementById("preview-window").contentWindow.document;
-    frame.open();
-    frame.write(htmlCode+cssCode+jsCode);
-    frame.close();
+function showPreview() {
+  var htmlCode = document.getElementById("htmlCode").value;
+  var cssCode = "" + document.getElementById("cssCode").value + "";
+  var jsCode = "" + document.getElementById("jsCode").value + "";
+  var frame = document.getElementById("preview-window").contentWindow.document;
+  frame.open();
+  frame.write(htmlCode + cssCode + jsCode);
+  frame.close();
+
+  autoSave();
 }
 
-function show(x){
-    document.getElementById("html").style.display="none";
-    document.getElementById("css").style.display="none";
-    document.getElementById("js").style.display="none";
-    document.getElementById("result").style.display="none";
-    document.getElementById(x).style.display="block";
+function show(x) {
+  document.getElementById("html").style.display = "none";
+  document.getElementById("css").style.display = "none";
+  document.getElementById("js").style.display = "none";
+  document.getElementById("result").style.display = "none";
+  document.getElementById(x).style.display = "block";
 }
 
-function show_all(){
-    if(window.innerWidth>=992)
-    {
-        document.getElementById("html").style.display="block";
-        document.getElementById("css").style.display="block";
-        document.getElementById("js").style.display="block";
-        document.getElementById("result").style.display="block";
-    }
-    if(window.innerWidth<992 && document.getElementById("html").style.display=="block")
-    {
-        document.getElementById("css").style.display="none";
-        document.getElementById("js").style.display="none";
-        document.getElementById("result").style.display="none";
-    }
+function show_all() {
+  if (window.innerWidth >= 992) {
+    document.getElementById("html").style.display = "block";
+    document.getElementById("css").style.display = "block";
+    document.getElementById("js").style.display = "block";
+    document.getElementById("result").style.display = "block";
+  }
+  if (
+    window.innerWidth < 992 &&
+    document.getElementById("html").style.display == "block"
+  ) {
+    document.getElementById("css").style.display = "none";
+    document.getElementById("js").style.display = "none";
+    document.getElementById("result").style.display = "none";
+  }
 }
+
+function autoSave() {
+  const htmlCode = document.getElementById("htmlCode").value;
+  const cssCode = document.getElementById("cssCode").value;
+  const jsCode = document.getElementById("jsCode").value;
+
+  const codeData = {
+    html: htmlCode,
+    css: cssCode,
+    js: jsCode,
+  };
+  localStorage.setItem("codeData", JSON.stringify(codeData));
+}
+
+function autoLoad() {
+  const savedCode = localStorage.getItem("codeData");
+
+  if (savedCode) {
+    const codeData = JSON.parse(savedCode);
+    document.getElementById("htmlCode").value = codeData.html;
+    document.getElementById("cssCode").value = codeData.css;
+    document.getElementById("jsCode").value = codeData.js;
+
+    showPreview(); // Update the preview after loading the saved code
+  }
+}
+
+window.onload = function () {
+  autoLoad();
+};
